229fd29a99cb9ea2c6513d219606bb976247179c
gabrielegr <00199518@uca.edu.sv>
2019-11-25 12:58:58 -0600

    adding more routes

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 	//Log In
-	let email_field = document.querySelector("#email_field")
+	let name_field = document.querySelector("#name_field")
 	let password_field = document.querySelector("#password_field")
 	let login_btn = document.querySelector("#login_btn")
 	login_btn.addEventListener("click", (e) => {
 		e.preventDefault()		
-		let email = email_field.value
+		let email = name_field.value
 		let password = password_field.value
 		if(email == "" || password == ""){
 			alert("Por favor llene todos los campos")
 		}
 		else {
 			if (true) {
-				location.href = "login.html";
+				location.href = "/login";
 			}
 			else {
 				alert("Usuario o Contraseña incorrectos")
