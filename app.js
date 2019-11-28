13bee4fd1c5e5e46c83119f9dafcbcf2d2982622
gabrielegr <00199518@uca.edu.sv>
2019-11-27 18:49:43 -0600

    Heroku time

--- app.js
+++ app.js
@@ -1,61 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookie = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var debug = require('debug')('proyectowebv2:database');
-var session=require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
-var superviseRouter = require('./routes/supervise');
-var reserveRouter = require('./routes/reserve');
-var morgan=require('morgan');
-var flash=require('connect-flash');
-var bodyParser=require('body-parser');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookie = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var debug = require("debug")("proyectowebv2:database");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
+var indexRouter = require("./routes/index");
+var userRouter = require("./routes/user");
+var superviseRouter = require("./routes/supervise");
+var reserveRouter = require("./routes/reserve");
+var morgan = require("morgan");
+var flash = require("connect-flash");
+var bodyParser = require("body-parser");
 var app = express();
-mongoose.connect(process.env.MONGO_URI, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    debug("success Connected to database")
+    debug("success Connected to database");
   })
-  .catch((err) => {
+  .catch(err => {
     debug(err);
     process.exit(1);
-  })
-  var db = mongoose.connection;
+  });
+var db = mongoose.connection;
 
-  app.use(session({
+app.use(
+  session({
     store: new MongoStore({
-        mongooseConnection: db,
-        collection: 'session',
+      mongooseConnection: db,
+      collection: "session",
+      resave: true,
+      secret: "uwu",
+      proxy: true,
+      resave: true,
+      saveUninitialized: true
     })
-  }));
-
-
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(morgan('dev'));
+app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookie());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(flash())
-app.use('/', indexRouter);
-
+app.use(flash());
+app.use("/", indexRouter);
 
 //static files
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -65,11 +69,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
